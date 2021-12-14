387b38dc134262b8ef372f323fcaa3e67d5a26ab
RohithBoppey <rohithboppey1298@gmail.com>
2021-12-14 11:11:10 +0530

    Final Commit

---
+++ Math.py
@@ -0,0 +1,14 @@
+l = [1,23,4,5,56,7]
+print(sum(l))
+
+a = 25.56777799999
+
+print(round(a, 4))
+
+import random
+
+print(random.random()) # always in 0 and 1
+
+# for choices kind of thing -> use random.choice(list)
+
+list = [1, 2, 3, 4, 5, 6]
---
+++ SnakeAndLadder.py
@@ -0,0 +1,87 @@
+import random
+size = 100
+
+current = 1
+
+snake = [32, 36, 48, 62, 88, 95, 97]
+ladder = [1, 4, 8, 10, 28, 21, 50, 71, 88]
+
+
+def checkSnake(current):
+    if (current == 32):
+        print("                                                     Snake")
+        current = 10
+    if (current == 36):
+        print("                                                     Snake")
+        current = 16
+    if (current == 48):
+        print("                                                     Snake")
+        current = 26
+    if (current == 62):
+        print("                                                     Snake")
+        current = 18
+    if (current == 88):
+        print("                                                     Snake")
+        current = 24
+    if (current == 95):
+        print("                                                     Snake")
+        current = 56
+    if (current == 97):
+        print("                                                     Snake")
+        current = 78
+    return current
+
+
+def checkLadder(current):
+    if (current == 4):
+        print("                                                     Ladder")
+        current = 14
+    if (current == 1):
+        print("                                                     Ladder")
+        current = 38
+    if (current == 8):
+        print("                                                     Ladder")
+        current = 10
+    if (current == 28):
+        print("                                                     Ladder")
+        current = 76
+    if (current == 21):
+        print("                                                     Ladder")
+        current = 42
+    if (current == 50):
+        print("                                                     Ladder")
+        current = 67
+    if (current == 71):
+        print("                                                     Ladder")
+        current = 92
+    if (current == 88):
+        print("                                                     Ladder")
+        current = 99
+    return current
+
+
+tries = 0
+
+print("Starting point: ", current, "\n\n")
+key = random.randint(1, 6)
+while(current <= size and current + key < size):
+    key = random.randint(1, 6)
+    current += key
+    print("Key: ", key)
+    current = checkSnake(current)
+    current = checkLadder(current)
+    print("Current: ", current)
+    print("\n")
+    tries = tries + 1
+
+while(current != size and current < size):
+    i = random.randint(1, 6)
+    print("Key: ", i)
+    if(current + i == size):
+            current = current + i
+            print("Current: ", current)
+            break
+    print("\n")
+    tries = tries + 1
+
+print("Game Done!, Your tries are: ", tries)
